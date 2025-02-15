c20a0370332ae084e2bf024b2a46e3223d2b199e
Dharam8218 <101041058+Dharam8218@users.noreply.github.com>
2025-02-15 19:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+const increment = document.getElementById("incrementBtn");
+
+let count = 0;
+
+increment.addEventListener("click",()=>{
+
+	alert(count);
+	count++;
+	counter.textContent=count;
+});
